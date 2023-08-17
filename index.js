b3e4534eb8d3238c54a911eb5013e027a31d8d45
gnarzulllayev <gnarzulllayev2000@gmail.com>
2023-08-17 19:37:59 +0500

    add project

--- index.js
+++ index.js
@@ -17,8 +17,12 @@ async function sendTelegramMessage(botToken, chatId, message) {
     }
 }
 
-const botToken = "6319657635:AAHV1daDS433iiqEluBdjV2IJk2ZCmX9dug";
-const chatId = "1377337356";
+// const botToken = "6319657635:AAHV1daDS433iiqEluBdjV2IJk2ZCmX9dug";
+// const chatId = "1377337356";
+// const message = 'Integration tests failed! Check the build.';
+
+const botToken = process.argv[2];
+const chatIds = process.argv.slice(3);
 const message = 'Integration tests failed! Check the build.';
 
 sendTelegramMessage(botToken, chatId, message).then();
